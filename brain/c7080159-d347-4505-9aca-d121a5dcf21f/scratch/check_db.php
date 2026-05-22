e77b82aa07e0f6bf49299a89cd4b45bec80986fb
toan <hoangvantoan17092006@gmail.com>
2026-05-22 14:43:49 +0700

    Xóa file không cần

--- brain/c7080159-d347-4505-9aca-d121a5dcf21f/scratch/check_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-
-foreach (\App\Models\ProductRecommendation::all() as $rec) {
-    echo "Product #{$rec->product_id} -> Rec #{$rec->recommended_product_id} ({$rec->type}) Created at: {$rec->created_at}" . PHP_EOL;
-}
